ef74059c93898f6021909a8759e0951dfb781512
Joshua Prodahl <jprodahl@gmail.com>
2017-01-15 19:43:45 -0600

    progress

--- src/BlockGravityPlugin.java
+++ src/BlockGravityPlugin.java
@@ -2,7 +2,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.plugin.PluginManager;
 
-public class SupportedBlocksPlugin extends JavaPlugin {
+public class BlockGravityPlugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		PluginManager manager = this.getServer().getPluginManager();
