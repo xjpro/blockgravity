25c3b08c3c82d68d9d8c4d9ef9e5914ab2017d7f
Joshua Prodahl <jprodahl@gmail.com>
2017-01-19 00:37:25 -0600

    support blocks burning and pistons moving them; intellij

--- src/blockgravity/BlockGravityPlugin.java
+++ src/blockgravity/BlockGravityPlugin.java
@@ -1,14 +1,13 @@
 package blockgravity;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlockGravityPlugin extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        PluginManager manager = this.getServer().getPluginManager();
-        manager.registerEvents(new BlockListener(), this);
-    }
+	@Override
+	public void onEnable() {
+		PluginManager manager = this.getServer().getPluginManager();
+		manager.registerEvents(new BlockListener(), this);
+	}
 }
